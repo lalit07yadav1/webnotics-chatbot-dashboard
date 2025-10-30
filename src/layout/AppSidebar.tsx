1d724fdd9a81d324627b96bc5a61eed34affaa7c
lalit07yadav <lalit07yadav@gmail.com>
2025-10-30 15:15:52 +0530

    updated login

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -3,17 +3,9 @@ import { Link, useLocation } from "react-router";
 
 // Assume these icons are imported from an icon library
 import {
-  BoxCubeIcon,
-  CalenderIcon,
   ChevronDownIcon,
   GridIcon,
   HorizontaLDots,
-  ListIcon,
-  PageIcon,
-  PieChartIcon,
-  PlugInIcon,
-  TableIcon,
-  UserCircleIcon,
 } from "../icons";
 import { useSidebar } from "../context/SidebarContext";
 
