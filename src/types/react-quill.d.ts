12ce79e576e4f2c590869b4f8a14fd7ea7969051
ultimocoder <ultimoresq@gmail.com>
2025-11-24 13:37:57 +0530

    Fix @types/react-quill not found error
    - Removed @types/react-quill from devDependencies (package doesn't exist in npm registry)
    - Created local type declaration file for react-quill in src/types/react-quill.d.ts
    - This resolves the 404 error during npm install on Render

---
+++ src/types/react-quill.d.ts
@@ -0,0 +1,26 @@
+declare module 'react-quill' {
+  import { Component } from 'react';
+
+  export interface ReactQuillProps {
+    value?: string;
+    defaultValue?: string;
+    placeholder?: string;
+    readOnly?: boolean;
+    theme?: string;
+    modules?: any;
+    formats?: string[];
+    bounds?: string | HTMLElement;
+    onChange?: (content: string, delta: any, source: string, editor: any) => void;
+    onChangeSelection?: (range: any, source: string, editor: any) => void;
+    onFocus?: (range: any, source: string, editor: any) => void;
+    onBlur?: (previousRange: any, source: string, editor: any) => void;
+    onKeyPress?: (event: any) => void;
+    onKeyDown?: (event: any) => void;
+    onKeyUp?: (event: any) => void;
+    className?: string;
+    style?: React.CSSProperties;
+  }
+
+  export default class ReactQuill extends Component<ReactQuillProps> {}
+}
+
